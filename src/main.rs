63cbb2dbbe60b3b92c27ca1db89ed2067e6ba9d7
Gaurav <allmanpride@gmail.com>
2022-07-24 17:26:47 -0400

    Add count argument; minor refactor

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ struct Cli {
     /// Use Primary Selection instead of Clipboard
     #[clap(short, long, action)]
     primary: bool,
-    /// Character between two entries
+    /// Separator between two entries for output
     #[clap(short, long, default_value = "\n")]
     separator: String,
     /// Do not clear output file before writing to it
@@ -28,6 +28,9 @@ struct Cli {
     /// Refresh Rate in miliseconds
     #[clap(short, long, default_value = "200")]
     refresh_rate: u64,
+    /// Only capture this many times, 0 for infinity
+    #[clap(short='n', long, default_value = "0")]
+    count: u64,
 }
 
 
@@ -40,14 +43,8 @@ fn main() {
 	clip = LinuxClipboardKind::Primary;
     }
     
-    let out_to_file = if !args.output.as_os_str().is_empty() {
-	true
-    } else {
-	false
-    };
-    
     let mut file:Option<fs::File> = None;
-    if out_to_file {
+    if !args.output.as_os_str().is_empty() {
 	file = Some(fs::OpenOptions::new()
 		    .write(true)
 		    .create(true)
@@ -58,15 +55,18 @@ fn main() {
     }
     let mut ctx = Clipboard::new().unwrap();
     let mut clip_txt = ctx.get_text_with_clipboard(clip).unwrap_or_else(|_| String::from(""));
+
+    let mut counter = 0;
     loop {
 	let clip_new = ctx.get_text_with_clipboard(clip).unwrap_or_else(|_| String::from(""));
 
 	if clip_new != clip_txt {
+
 	    if !args.quiet {
 		print!("{}{}", clip_new, args.separator);
 		io::stdout().flush().unwrap();
 	    }
-	    if out_to_file{
+	    if !file.is_none(){
 		file.as_ref().unwrap().write_all(clip_new.as_bytes()).expect("Unable to write to file.");
 		file.as_ref().unwrap().write_all(args.separator.as_bytes()).expect("Unable to write to file.");
 	    }
@@ -74,7 +74,14 @@ fn main() {
 		let mut cmd = Command::new(args.command.clone());
 		cmd.arg(clip_new.clone()).output().expect("Command Failed.");
 	    }
+
 	    clip_txt = clip_new;
+	    counter += 1;
+	    if counter == args.count {
+		// counter is never 0 here as it starts from 1 as soon
+		// as a match is found.
+		break;
+	    }
 	}
 	thread::sleep(time::Duration::from_millis(args.refresh_rate));
     }
